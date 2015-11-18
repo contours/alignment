366f206ee433a3146f7afc0d60522df20b609cf7
Ryan Shaw <ryanshaw@unc.edu>
2015-11-18 16:24:26 -0500

    check that tokens match words

--- src/main/java/in/aesh/align/Transcript.java
+++ src/main/java/in/aesh/align/Transcript.java
@@ -1,5 +1,6 @@
 package in.aesh.align;
 
+import com.google.common.base.CharMatcher;
 import com.google.common.collect.ImmutableList;
 import edu.cmu.sphinx.util.TimeFrame;
 import java.util.List;
@@ -33,7 +34,35 @@ class Transcript {
                 .collect(Collectors.toList());
     }
     
+    private void checkTokens(List<String> tokens) {
+        List<Word> words = getWords();
+        IntStream.range(0, Math.max(words.size(), tokens.size())).forEach(i -> {
+            if (i == words.size()) {
+                throw new RuntimeException("More tokens than words");
+            }
+            if (i == tokens.size()) {
+                throw new RuntimeException("More words than tokens");
+            }
+            String token1 = CharMatcher.JAVA_LETTER_OR_DIGIT
+                    .retainFrom(words.get(i).token)
+                    .toLowerCase();
+            String token2 = CharMatcher.JAVA_LETTER_OR_DIGIT
+                    .retainFrom(tokens.get(i))
+                    .toLowerCase();
+            if (! token1.equals(token2)) {
+                System.err.println(tokens);
+                IntStream.range(0, i+1).forEach(j -> {
+                    System.err.println(j + " |" + words.get(j).token + "|" + tokens.get(j) + "|");
+                });
+                throw new RuntimeException("mismatch at " + i + ": "
+                        + "|" 
+                        + token1 + "|" + token2 + "|");
+            }
+        });
+    }
+    
     public Transcript align(List<String> tokens, AlignmentResults results, long duration) {
+        checkTokens(tokens);
         List<Word> words = getWords();
         assert words.size() == tokens.size();
         assert tokens.size() == results.indexes.length;
